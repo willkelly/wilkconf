aa9f07a3631438996dfe2632305630d9d95746ac
William Kelly <the.william.kelly@gmail.com>
2013-07-29 12:37:45 -0500

    fix sed for mail_location

--- initial/dovecot-setup.sh
+++ initial/dovecot-setup.sh
@@ -4,7 +4,7 @@ if ! dpkg -l dovecot-imapd &>/dev/null; then
     sudo apt-get install -y dovecot-imapd
     sudo /etc/init.d/dovecot stop
     echo 'ENABLED=0' | sudo tee -a /etc/default/dovecot
-    sudo sed -i 's/^mail_location = .*/mail_location = maildir:~\/Maildir\/%u/$' \
+    sudo sed -i 's/^mail_location = .*/mail_location = maildir:~\/Maildir\/%u/g' \
 	/etc/dovecot/conf.d/10-mail.conf
 elif ! grep 'mail_location = maildir:~\/Maildir\/%u' \
 	/etc/dovecot/conf.d/10-mail.conf &>/dev/null; then
