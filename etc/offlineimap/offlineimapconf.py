394b17a89cee6a4d4808ee56fe894a5f6cf74571
William Kelly <the.william.kelly@gmail.com>
2014-02-10 16:44:55 -0600

    Cleaner monkey patching of offlineimap with more gnus support.

--- etc/offlineimap/offlineimapconf.py
+++ etc/offlineimap/offlineimapconf.py
@@ -21,40 +21,13 @@ def get_user_emacs(host, port):
 
 # stolen from http://roland.entierement.nu/blog/2010/09/08/gnus-dovecot-offlineimap-search-a-howto.html
 
-# Propagate gnus-expire flag
-from offlineimap import imaputil
-
-def lld_flagsimap2maildir(flagstring):
-    flagmap = {'\\seen': 'S',
-               '\\answered': 'R',
-               '\\flagged': 'F',
-               '\\deleted': 'T',
-               '\\draft': 'D',
-               'gnus-expire': 'E'}
-    retval = []
-    imapflaglist = [x.lower() for x in flagstring[1:-1].split()]
-    for imapflag in imapflaglist:
-        if flagmap.has_key(imapflag):
-            retval.append(flagmap[imapflag])
-        retval.sort()
-    return retval
-
-def lld_flagsmaildir2imap(list):
-    flagmap = {'S': '\\Seen',
-               'R': '\\Answered',
-               'F': '\\Flagged',
-               'T': '\\Deleted',
-               'D': '\\Draft',
-               'E': 'gnus-expire'}
-    retval = []
-    for mdflag in list:
-        if flagmap.has_key(mdflag):
-            retval.append(flagmap[mdflag])
-    retval.sort()
-    return '(' + ' '.join(retval) + ')'
-
-imaputil.flagsmaildir2imap = lld_flagsmaildir2imap
-imaputil.flagsimap2maildir = lld_flagsimap2maildir
+import offlineimap.imaputil as IU
+if not hasattr(IU, 'monkeypatchdone'):
+    IU.flagmap += [('gnus-expire','E'),
+                   ('gnus-dormant', 'Q'),
+                   ('gnus-save', 'V'),
+                   ('gnus-forward', 'W')]
+    IU.monkeypatchdone = True
 
 # Grab some folders first, and archives later
 high = ['^important$', '^work$']
