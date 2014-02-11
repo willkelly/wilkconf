132703c82745be5afc2a38029fa4cf4efcbdde26
William Kelly <the.william.kelly@gmail.com>
2014-02-10 21:18:28 -0600

    Update caldav -- STILL DANGEROUS

--- etc/emacs/autoload/Zcaldav-conf.el
+++ etc/emacs/autoload/Zcaldav-conf.el
@@ -1,6 +1,10 @@
+
 (setq org-caldav-url "https://rackermail.cannot.name/users")
-(setq org-caldav-inbox "/home/will/doc/caldav.org")
-(setq org-caldav-files '("/home/will/doc/agenda.org"))
+(setq org-caldav-inbox "/home/will/doc/org/agenda/caldav.org")
+(setq org-caldav-files '("/home/will/doc/org/agenda/agenda.org"))
 (setq org-caldav-calendar-id "william.kelly%40rackspace.com/calendar")
 (setq org-caldav-uuid-extension ".EML")
+(setq org-import-icalendar-filename  org-caldav-inbox)
+
+;(setq org-caldav-sync-changes-to-org 'all)
 ;(setq org-icalendar-timezone "america/chicago")
