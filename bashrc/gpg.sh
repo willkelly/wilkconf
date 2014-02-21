0ff604fb553d7ec4c73f04ed90d3a390aaeae114
William Kelly <the.william.kelly@gmail.com>
2014-02-20 22:34:21 -0600

    Reformat long command

--- bashrc/gpg.sh
+++ bashrc/gpg.sh
@@ -6,16 +6,15 @@ GPG_TTY=$(tty)
 export GPG_TTY
 if ! pgrep gpg-agent >/dev/null; then
     gpg-agent --daemon --enable-ssh-support \
-        --write-env-file "${HOME}/.gpg-agent-info" &>/dev/null
+	--write-env-file "${HOME}/.gpg-agent-info" &>/dev/null
 fi
 
-for f in "${HOME}/.gpg-agent-info" "${HOME}/.gnupg/gpg-agent-info-${HOSTNAME}"; do
+for f in "${HOME}/.gpg-agent-info" \
+    "${HOME}/.gnupg/gpg-agent-info-${HOSTNAME}"; do
     if [ -f "${f}" ]; then
 	. "${f}"
     export GPG_AGENT_INFO
     export SSH_AUTH_SOCK
     fi
 done
-
 ## END ENCRYPTION BLOCK
-
