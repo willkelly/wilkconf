f8b7174c0a61b16ceee1a110f09fd2145dab40d5
William Kelly <the.william.kelly@gmail.com>
2015-05-07 12:04:13 -0500

    Update whitespace preferences.

--- etc/emacs/autoload/whitespace.el
+++ etc/emacs/autoload/whitespace.el
@@ -4,3 +4,7 @@ indentation::space space-before-tab::space space-after-tab::space))
 (setq-default whitespace-line-column 80)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (global-whitespace-mode 1)
+(setq-default c-default-style "k&r"
+              c-basic-offset 4
+              tab-width 8
+              indent-tabs-mode nil)
